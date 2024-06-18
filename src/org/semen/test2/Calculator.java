cf3f9996aef0bd080a89dbd8986854f13bc20e63
Ksaumon <97893722+ksaumon@users.noreply.github.com>
2024-06-18 23:30:26 +0300

    Исправление ошибок

--- src/org/semen/test2/Calculator.java
+++ src/org/semen/test2/Calculator.java
@@ -21,13 +21,32 @@ public class Calculator {
             throw new Exception("Некорректный знак действия");
         }
 
+        if (!data[0].startsWith("\"") || !data[0].endsWith("\"")) {
+            throw new Exception("Первым аргументом выражения, подаваемого на вход, должна быть строка. Пример \"Dad\"");
+        }
+
         if (action == '*' || action == '/') {
             if (data[1].contains("\"")) throw new Exception("Строчку можно делить или умножать только на число");
         }
 
         for (int i = 0; i < data.length; i++) {
             data[i] = data[i].replace("\"", "");
+            if (data[i].length() > 10) {
+                throw new Exception("Строка длиннее 10 символов");
+            }
         }
+
+        if (action == '*' || action == '/') {
+            if (!isInteger(data[1])) {
+                throw new Exception("Калькулятор умеет работать только с целыми числами.");
+            }
+            int number = Integer.parseInt(data[1]);
+            if (number < 1 || number > 10) {
+                throw new Exception("Число должно быть от 1 до 10");
+            }
+        }
+
+
 
         switch (action) {
             case '+':
@@ -63,5 +82,24 @@ public class Calculator {
     private String divideString(String str, int divisor) {
         int newLen = str.length() / divisor;
         return str.substring(0, newLen);
+    }
+
+    public static boolean isInteger(String str) {
+        if (str == null || str.isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (i == 0 && str.charAt(i) == '-') {
+                if (str.length() == 1) {
+                    return false;
+                } else {
+                    continue;
+                }
+            }
+            if (!Character.isDigit(str.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/org/semen/test2/StringUtils.java
+++ src/org/semen/test2/StringUtils.java
@@ -2,6 +2,10 @@ package org.semen.test2;
 
 public class StringUtils {
     public static void printInQuotes(String text) {
-        System.out.println("\"" + text + "\"");
+        if (text.length() > 40) {
+            System.out.println(text.substring(0, 40) + "...");
+        } else {
+            System.out.println("\"" + text + "\"");
+        }
     }
 }
